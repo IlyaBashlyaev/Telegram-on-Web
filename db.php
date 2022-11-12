7dd729b59eb3477c3f85104d8b81a1ad044d35d7
Ilya Bashlyaev <ibashlyaev2000@gmail.com>
2022-11-12 13:42:25 +0100

    Commit

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
     if ($_SERVER['HTTP_HOST'] == 'telegram-on-web.herokuapp.com')
-        $connection = new mysqli('sql7.freesqldatabase.com', 'sql7530907', 'lfzWgEg88G', 'sql7530907');
+        $connection = new mysqli('bav0fegt5cf833tuhm3z-mysql.services.clever-cloud.com', 'unhpmpdyf3fnpsgo', 'ieI44C230bJDDc1Rdtjg', 'bav0fegt5cf833tuhm3z');
     else
         $connection = new mysqli('127.0.0.1', 'Ilya Bashlyaev', '#vOV(0y2#vOV(0y2', 'chats-db');
 ?>
